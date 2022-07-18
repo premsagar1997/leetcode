f608dfa915ec07a7783c488ef7a91c206f7a9acd
Premsagar Choudhary <76701517+premsagar1997@users.noreply.github.com>
2022-07-18 10:43:27 +0530

    Create leetcode#35.cpp

---
+++ 1-100/leetcode#35.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+        
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (target < nums[m]) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        
+        return l;
+    }
+};
