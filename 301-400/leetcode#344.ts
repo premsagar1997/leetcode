d82921f2679ce0c87636eba689a398776d139b8c
Premsagar Choudhary <76701517+premsagar1997@users.noreply.github.com>
2024-02-13 10:43:12 +0530

    Update leetcode#344.ts

--- 301-400/leetcode#344.ts
+++ 301-400/leetcode#344.ts
@@ -1,3 +1,4 @@
+// Typescript solution
 function reverseString(s: string[]): void {
     let l: number = 0;
     let r: number = s.length - 1;
@@ -8,5 +9,21 @@ function reverseString(s: string[]): void {
         s[r] = t;
         l++;
         r--;
+    }
+};
+
+//c++ solution
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l = 0, r = s.size() - 1;
+
+        while (l < r) {
+            int t = s[l];
+            s[l] = s[r];
+            s[r] = t;
+            l++;
+            r--;
+        }
     }
 };
