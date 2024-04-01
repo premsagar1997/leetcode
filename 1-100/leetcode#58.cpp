bbfb6b479d8b935db60079ac95e327213a28b6c6
Premsagar Choudhary <76701517+premsagar1997@users.noreply.github.com>
2024-04-01 14:13:41 +0530

    Update leetcode#58.cpp

--- 1-100/leetcode#58.cpp
+++ 1-100/leetcode#58.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int i = s.length() - 1;
         int count = 0;
-        
-        for (; i >= 0; i--) {
-            if (s[i] != ' ')
-                break;
-        }
-        
-        for (; i >= 0; i--) {
-            if (s[i] != ' ')
+        bool isCounting = false;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
                 count++;
-            else
+                isCounting = true;
+            } else if (isCounting) {
                 break;
+            }
         }
-        
+
         return count;
     }
 };
