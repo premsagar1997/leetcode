54fe7370efd308fe4a28cde3dee33c0ad98e086f
Premsagar Choudhary <76701517+premsagar1997@users.noreply.github.com>
2025-02-09 16:23:00 +0530

    Update leetcode#509.cpp

--- 501-600/leetcode#509.cpp
+++ 501-600/leetcode#509.cpp
@@ -14,3 +14,24 @@ public:
         return f1;
     }
 };
+
+
+// Method 2: Using DP
+class Solution {
+    public:
+    int solve(int n, vector<int> &dp) {
+        // BASE CASE
+        if (n <= 1) return n;
+
+        if (dp[n] != -1) return dp[n];
+
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    }
+
+    int fib(int n) {
+        vector<int> dp(31, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        return solve(n, dp);
+    }
+};
