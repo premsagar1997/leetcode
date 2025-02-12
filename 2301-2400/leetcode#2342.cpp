f98844daba9cf6e9db59b7eebdd399541900a353
Premsagar Choudhary <76701517+premsagar1997@users.noreply.github.com>
2025-02-12 23:00:53 +0530

    Update leetcode#2342.cpp

--- 2301-2400/leetcode#2342.cpp
+++ 2301-2400/leetcode#2342.cpp
@@ -1,3 +1,5 @@
+// Method 1
+
 class Solution {
 public:
     int sumOfDigit(int n) {
@@ -22,6 +24,37 @@ public:
                 mp[sum] = max(nums[i], mp[sum]);
             } else {
                 mp[sum] = nums[i];
+            }
+        }
+
+        return result;
+    }
+};
+
+// Method 2
+class Solution {
+public:
+    int sumOfDigit(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int r = n % 10;
+            sum += r;
+            n = n / 10;
+        }
+        return sum;
+    }
+
+    int maximumSum(vector<int>& nums) {
+        int arr[82] = {0};
+        int result = -1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int sum = sumOfDigit(nums[i]);
+            if (arr[sum] != 0) {
+                result = max(result, nums[i] + arr[sum]);
+                arr[sum] = max(nums[i], arr[sum]);
+            } else {
+                arr[sum] = nums[i];
             }
         }
 
