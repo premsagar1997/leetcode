b58f87bd3fc827e525064b24b1739fff64d3ce54
Premsagar Choudhary <76701517+premsagar1997@users.noreply.github.com>
2025-09-03 22:11:30 +0530

    Update leetcode#26.cpp

--- 1-100/leetcode#26.cpp
+++ 1-100/leetcode#26.cpp
@@ -12,3 +12,25 @@ public:
         return (count + 1);
     }
 };
+
+// --------------------------------
+// Using Two Pointers approach:
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        int j = 1;
+
+        while (j < n) {
+            if (nums[i] != nums[j]) { //if both elements are not same
+                i++;
+                nums[i] = nums[j];
+            }
+
+            j++; //look for next unique element in array
+        }
+
+        return i + 1;
+    }
+};
