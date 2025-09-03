4f0db095338953ce5e970d95399a1248049cd810
Premsagar Choudhary <76701517+premsagar1997@users.noreply.github.com>
2025-09-03 22:45:24 +0530

    Update leetcode#151.java

--- 101-200/leetcode#151.java
+++ 101-200/leetcode#151.java
@@ -12,3 +12,41 @@ class Solution {
         return sb.toString().trim();
     }
 }
+
+// ---------------------
+// Using Two Pointers
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+
+        int i = 0;
+        int l = 0;
+        int r = 0;
+        int n = s.length();
+
+        while (i < n) {
+            // assign s[r] = s[i] until space encounter
+            while (i < n && s[i] != ' ') {
+                s[r] = s[i];
+                i++;
+                r++;
+            }
+
+            // check is l is less than r
+            if (l < r) {
+                // reverse sub string from l to r
+                reverse(s.begin() + l, s.begin() + r);
+                s[r] = ' '; // after sub string reverse, add space
+                r++;
+
+                l = r; // move l pointer to r
+            }
+
+            i++;
+        }
+
+        s = s.substr(0, r - 1);
+        return s;
+    }
+};
